2435cf5537bdaf5f4e4a7a2fd566ce79c4724683
Marcin Kosiński <mkosinski@prodataconsult.pl>
2021-09-14 14:29:36 +0200

    reduce image size

--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -14,21 +14,21 @@ export const Hero: React.FC<HeroProps> = ({ title, subtitle, subsubtitle }) => {
   const { mobileImage, desktopImage, bigDesktopImage } = useStaticQuery(
     graphql`
       query {
-        mobileImage: file(relativePath: { eq: "whyr-big-bg-stock.png" }) {
+        mobileImage: file(relativePath: { eq: "whyr-big-bg-stock.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 900, quality: 90) {
               ...GatsbyImageSharpFluid
             }
           }
         }
-        desktopImage: file(relativePath: { eq: "whyr-big-bg-stock.png" }) {
+        desktopImage: file(relativePath: { eq: "whyr-big-bg-stock.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 1700, quality: 90) {
               ...GatsbyImageSharpFluid
             }
           }
         }
-        bigDesktopImage: file(relativePath: { eq: "whyr-big-bg-stock.png" }) {
+        bigDesktopImage: file(relativePath: { eq: "whyr-big-bg-stock.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 2400, quality: 95) {
               ...GatsbyImageSharpFluid
